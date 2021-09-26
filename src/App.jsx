90b6dd54c641193e464876134e84e7d8774a44cf
Nick Boyadjian <nboyadjian95@gmail.com>
2021-09-26 18:23:54 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import Navbar from './components/navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
